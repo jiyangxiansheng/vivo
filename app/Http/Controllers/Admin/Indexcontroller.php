e8d16e8fbb4a1725b990365b8ffb4456249d82da
jiyangxiansheng <jiyangxiansheng@163.com>
2019-04-22 10:25:09 +0800

    111

--- app/Http/Controllers/Admin/Indexcontroller.php
+++ app/Http/Controllers/Admin/Indexcontroller.php
@@ -7,10 +7,10 @@ use App\Http\Controllers\Controller;
 
 class Indexcontroller extends Controller
 {
-  
+  		//后台首页
     	public function index()
     	{
     		return view('admin.index');
     	}
-    
+
 }
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,22 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+//前台路由组
+Route::group([], function(){
+	//前台首页
+	Route::get('/', 'Home\IndexController@index');
+});
 
-Route::get('/', 'Home\IndexController@index');
 
 //后台登录页面
 //
 Route::group([],function () {
-
+	//后台首页
 	Route::get('/admins','Admin\IndexController@index');
+	//商品分类
+	Route::get('/admin/cates','Admin\GoodsController@cates');
+	//添加分类
+	Route::get('/admin/cates/create','Admin\GoodsController@create');
+	//修改分类
+	Route::get('/admin/cates/edit','Admin\GoodsController@edit');
 });
